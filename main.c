344f07967e12f7ba91d3fcb34de6f7721925bdb3
Deborah Jahaj <deborah.jahaj05@gmail.com>
2021-03-23 11:13:55 -0500

    Publish issue with arguments inspected

--- main.c
+++ main.c
@@ -335,25 +335,42 @@ int main(void)
                     putcUart0('\n');
                 }
             }
-            if (isCommand(&serialData, "PUBLISH", 2))
+            if (isCommand(&serialData, "PUBLISH", 3))
             {
                 uint8_t i = 0;
                 uint8_t topicNameLength = 0;
                 uint8_t dataNameLength = 0;
+
                 char * tempTopicName = getFieldString(&serialData, 1);
-                while (tempTopicName[topicNameLength] != '\0')
+                char * tempDataName = getFieldString(&serialData, 2);
+                char * extra = getFieldString(&serialData, 3);
+
+                putsUart0(tempTopicName);
+                putcUart0('\n');
+                putsUart0(tempDataName);
+                putcUart0('\n');
+                putsUart0(extra);
+
+                /*while (tempTopicName[topicNameLength] != '\0')
                     topicNameLength++;
                 char topicName[topicNameLength];
                 for (i = 0; i < topicNameLength; i++)
                     topicName[i] = tempTopicName[i];
-                char * tempDataName = getFieldString(&serialData, 2);
+
+                char *pTopic = topicName;
+
                 while (tempDataName[dataNameLength] != '\0')
                     dataNameLength++;
                 char dataName[dataNameLength];
                 for (i = 0; i < dataNameLength; i++)
                     dataName[i] = tempDataName[i];
 
-                mqttSendPublish(data, topicName, dataName);
+                char*pData = dataName;*/
+
+                //putsUart0(pTopic);
+                //putsUart0(pData);
+
+                //mqttSendPublish(data, tempTopicName,tempDataName);
             }
             if (isCommand(&serialData, "SUBSCRIBE", 1))
             {
